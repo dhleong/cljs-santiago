cc5f5e518f35682dd80a98af01f22b308e22c01d
Daniel Leong <me@dhleong.net>
2020-03-29 09:55:04 -0400

    Support input[type=checkbox] as a boolean value

--- src/main/santiago/input.cljs
+++ src/main/santiago/input.cljs
@@ -4,14 +4,44 @@
                                    dispatch-change
                                    remove-shared-keys]]))
 
+(defn- input-type-symbol
+  ([opts _] (input-type-symbol opts))
+  ([opts]
+   (when-let [t (:type opts)]
+     (cond
+       (symbol? t) t
+       (keyword? t) (symbol (name t))
+       (string? t) (symbol t)
+       :else (do (println "Unexpected input type" t)
+                 t)))))
+
+(def ^:private input-configs
+  {'checkbox {:value-key :checked
+              :get-value (fn [e] (.-checked (.-target e)))
+              :default false}
+   :default {:value-key :value
+             :get-value (fn [e] (.-value (.-target e)))
+             :default ""}})
+
+(defn- input-config [input-type]
+  (or (get input-configs input-type)
+      (:default input-configs)))
+
+
+; ======= public interface ================================
+
 (defcomponent input
   [context opts]
-  (let [on-change (fn [e]
-                    (let [new-value (.-value (.-target e))]
+  (let [input-type (input-type-symbol opts)
+        {:keys [get-value default value-key]} (input-config input-type)
+        on-change (fn [e]
+                    (let [new-value (get-value e)]
                       (dispatch-change context opts new-value)))
-        value (if-some [v (current-value context opts)] v "")]
+        value (if-some [v (current-value context opts)]
+                v
+                (get opts :default default))]
     [:input (-> opts
                 (assoc :on-change on-change
-                       :value value)
+                       value-key value)
                 remove-shared-keys)]))
 
